5102a7902a1bbe21bc686d2ba328fc49cb03ee13
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-06-17 11:51:07 -0400

    add docstrings

--- python_programs/loop_fire_finder.py
+++ python_programs/loop_fire_finder.py
@@ -1,10 +1,31 @@
+"""
+
+This program connects to the database. It pulls all of the counties in the eastern US from 
+the east_us_plot table. Then, for each county, it uses the east_us_cond table to find all 
+of the instances where a plot was reported to have fire damage in that county during the 
+study period (1980 to 2022).
+
+For this program to work, you need to have the plot, and cond tables for each state 
+downloaded from the FIA database. Those then need to be put into a local postgres database. 
+The state-by-state plot and cond tables will need to be merged into east_us_plot, and 
+east_us_cond tables respectively.
+
+This program accesses details for connecting to the database through a .env file. 
+
+"""
+
+# import libraries to help us connect to the database and access the .env file
 import psycopg2
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-output_contents = "statecd,unitcd,countycd,firedplots\n"
+# start the output contents by inserting the titles of each column in the output csv
+output_contents = "statecd,unitcd,countycd,burnedplots\n"
 
+# Connect to the database using details from the .env file
+# On your local computer, you can replace the os.getenv() calls with the details of your 
+# own database
 connection = psycopg2.connect(
     dbname = os.getenv("DBNAME"),
     user = os.getenv("USER"),
@@ -15,6 +36,7 @@ connection = psycopg2.connect(
 
 cursor = connection.cursor()
 
+# from the east_us_plot table, get the details of every county in the eastern US
 cursor.execute(f"""
     SELECT statecd, unitcd, countycd 
     FROM east_us_plot 
@@ -31,11 +53,13 @@ for county_row in counties:
     unitcd = county_row[1]
     countycd = county_row[2]
 
+    # print for monitoring purposes
     count += 1
     print(f'Processing {statecd}  {unitcd}  {countycd}. Count: {count}')
 
+    # count all instances where fire damage was reported on a plot in the county between 1980 and 2022 
     cursor.execute(f"""
-        SELECT  east_us_plot.statecd, east_us_plot.unitcd, east_us_plot.countycd, COUNT(east_us_plot.invyr) AS firedplots
+        SELECT  east_us_plot.statecd, east_us_plot.unitcd, east_us_plot.countycd, COUNT(east_us_plot.invyr) AS burnedplots
         FROM east_us_cond 
         LEFT JOIN east_us_plot
         ON east_us_cond.plt_cn = east_us_plot.cn
@@ -49,9 +73,9 @@ for county_row in counties:
         GROUP BY east_us_plot.statecd, east_us_plot.unitcd, east_us_plot.countycd;
     """)
 
-
     rows = cursor.fetchall()
 
+    # write the counties to output_contents
     if not rows:                        # no plots in the county had fire reported between 1980 and 2022 
         output_contents += f'{statecd},{unitcd},{countycd},0\n'
 
@@ -61,8 +85,11 @@ for county_row in counties:
             output_contents += str(county_row[3])
             output_contents += '\n' 
 
+# write output_contents to a csv file
+# replace the path with the location where you want the csv file to be written on your computer
 with open(f"C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/east_us_fire_occurreces.csv", "w") as output_file:
     output_file.write(output_contents)
 
+# clean up
 cursor.close()
 connection.close()
--- python_programs/shapefile_field_filler.py
+++ python_programs/shapefile_field_filler.py
@@ -1,3 +1,11 @@
+"""
+This program adds data from a csv file into a shapefile as fields on the layer.
+
+Replace the paths of the input csv, the input shapefile, and the output shapefile to match 
+the desired locations on your computer.
+
+"""
+
 import pandas as pd
 import geopandas as gpd
 from dotenv import load_dotenv
@@ -5,9 +13,11 @@ import os
 
 load_dotenv()
 
+# read in the input csv file
 csv_file_path = f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/east_us_percents_and_ratios_by_plot.csv'
 df = pd.read_csv(csv_file_path)
 
+# read in the input shapefile
 shapefile_path = f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/east_us_shapefile/east_us_shapefile.shp'
 gdf = gpd.read_file(shapefile_path)
 
@@ -16,8 +26,5 @@ gdf["COUNTYFP"] = gdf["COUNTYFP"].astype(int)
 
 gdf = gdf.merge(df, left_on=["STATEFP", "COUNTYFP"], right_on=["statecd", "countycd"], how="left")
 
-# DEBUGGING
-# print(gdf[["STATEFP", "COUNTYFP"]].head())
-# print(df[["statecd", "countycd"]].head())
-
+# output a new shapefile including the data from the input csv file as fields
 gdf.to_file(f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/east_us_shapefile.shp')
