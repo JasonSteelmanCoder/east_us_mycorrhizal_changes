1e88b1e93f45b2a21c0f854a23ff319a9a183fd5
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-07-01 17:06:51 -0400

    fill out lat_long_to_county.py

--- python_programs/lat_long_to_county.py
+++ python_programs/lat_long_to_county.py
@@ -1,39 +1,47 @@
 import requests
 import json
 import pandas as pd
+import csv
+import os
+from dotenv import load_dotenv
 
-# choose your variables
-site_name = "EHFenced1"
-latitude = "42.44318871"
-longitude = "-76.40273319"
+load_dotenv()
 
 # prepare to collect the data
-fips_dict = {"site_name": [], "fips": []}
+fips_dict = {"site_name": [], "fips": [], "statecd": [], "countycd": []}
 
-# connect to the API
-url = "https://geo.fcc.gov/api/census/block/find"
+# get the input data from the csv file
+with open(f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/phillips_us_sites.csv', 'r') as input_file:
+    reader = csv.reader(input_file)
+    for row in reader:
+        print(".", end="")
+        site_name = row[0]
+        latitude = row[1]
+        longitude = row[2]
 
-params = {
-    "latitude": latitude,
-    "longitude": longitude,
-    "format": "json",
-}
+        # call the API
+        url = "https://geo.fcc.gov/api/census/block/find"
 
-response = requests.get(url, params)
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "format": "json",
+        }
 
-# put the site name in the dictionary
-fips_dict["site_name"].append(site_name)
+        response = requests.get(url, params)
 
-# give the site a county or None
-if response.status_code == 200:
-    response_dict = json.loads(response.text)
-    fips_dict["fips"].append(response_dict["County"]["FIPS"])
-else:
-    print(f"Error on site {site_name}: {response.text}")
-    fips_dict["fips"].append(None)
+        # give the site a county or None
+        if response.status_code == 200:
+            response_dict = json.loads(response.text)
+            new_fips = response_dict["County"]["FIPS"]
+            fips_dict["site_name"].append(site_name)
+            fips_dict["fips"].append(new_fips)
+            fips_dict["statecd"].append(str(new_fips[:2]))
+            fips_dict["countycd"].append(str(new_fips[2:]).lstrip('0'))
+        else:
+            print(f"\nError on site {site_name}: {response.text}")
 
 # turn it all into a dataframe
 fips = pd.DataFrame(fips_dict)
 
-print('\n')
-print(fips)
+fips.to_csv(f'C:/Users/{os.getenv("MS_USER_NAME")}/Desktop/phillips_sites_fips.csv', index=False)
