947ba9058657fb8c1492b3a8da7363b437e075f1
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-07-03 10:37:25 -0400

    update queries in ecological_group_finder.py

--- python_programs/ecological_group_finder.py
+++ python_programs/ecological_group_finder.py
@@ -18,14 +18,41 @@ cursor = connection.cursor()
 
 # find if there are invasive species at all in each county
 cursor.execute(f"""
-    SELECT east_us_plot.statecd, east_us_plot.countycd, count(DISTINCT ecological_groups.ecological_group) > 0 AS has_invasives
-    FROM east_us_plot
-    LEFT JOIN worms_drake 
-    ON east_us_plot.statecd = worms_drake.statecd AND east_us_plot.countycd = worms_drake.countycd
-    LEFT JOIN ecological_groups
-    ON ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name
-    GROUP BY east_us_plot.statecd, east_us_plot.countycd
-    ORDER BY has_invasives;
+    WITH 
+    combos AS (
+        WITH 
+        phillips_locations AS (
+            SELECT phillips_sites_fips.site_name, originalspeciesbinomial, speciesbinomial, phillips_sites_fips.statecd, phillips_sites_fips.countycd
+            FROM phillips_sites_fips
+            LEFT JOIN worm_sp_occur_phillips 
+            ON phillips_sites_fips.site_name = worm_sp_occur_phillips.site_name
+            WHERE worm_sp_occur_phillips.native = 'Non-native'
+        )
+        -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they have an ecological group classification for invasive species)
+        SELECT 
+            east_us_counties.statecd, 
+            east_us_counties.countycd, 
+            ecological_groups.ecological_group,
+            COALESCE(ecological_groups.ecological_group IN ('Epigeic', 'Anecic', 'Endogeic', 'Epi-Endogeic'), false) AS is_invaded_row
+        FROM east_us_counties
+        LEFT JOIN worms_drake 
+        ON worms_drake.origin != 'Nearctic' AND east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+        LEFT JOIN chang_fips
+        ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+        LEFT JOIN phillips_locations
+        ON phillips_locations.statecd = east_us_counties.statecd AND phillips_locations.countycd = east_us_counties.countycd
+        LEFT JOIN ecological_groups
+        ON 
+            (ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+            OR (ecological_groups.species = chang_fips.species)
+            OR (LOWER(REGEXP_REPLACE(phillips_locations.speciesbinomial, '.*\s', '')) = LOWER(ecological_groups.species))
+        GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+        ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+    )
+    SELECT statecd, countycd, BOOL_OR(is_invaded_row) AS has_invasive
+    FROM combos
+    GROUP BY statecd, countycd
+    ORDER BY statecd, countycd;
 """)
 
 counties = cursor.fetchall()
@@ -36,18 +63,34 @@ output_1 = pd.DataFrame(counties, columns=["statecd", "countycd", "has_invasives
 cursor.execute(f"""
     WITH 
     combos AS (
+        WITH 
+        phillips_locations AS (
+            SELECT phillips_sites_fips.site_name, originalspeciesbinomial, speciesbinomial, phillips_sites_fips.statecd, phillips_sites_fips.countycd
+            FROM phillips_sites_fips
+            LEFT JOIN worm_sp_occur_phillips 
+            ON phillips_sites_fips.site_name = worm_sp_occur_phillips.site_name
+            WHERE worm_sp_occur_phillips.native = 'Non-native'
+        )
         -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they equal epigeic)
         SELECT 
-            east_us_plot.statecd, 
-            east_us_plot.countycd, 
+            east_us_counties.statecd, 
+            east_us_counties.countycd, 
+            ecological_groups.ecological_group,
             COALESCE(ecological_groups.ecological_group = 'Epigeic', false) AS is_epigeic_row
-        FROM east_us_plot
+        FROM east_us_counties
         LEFT JOIN worms_drake 
-        ON east_us_plot.statecd = worms_drake.statecd AND east_us_plot.countycd = worms_drake.countycd
+        ON worms_drake.origin != 'Nearctic' AND east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+        LEFT JOIN chang_fips
+        ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+        LEFT JOIN phillips_locations
+        ON phillips_locations.statecd = east_us_counties.statecd AND phillips_locations.countycd = east_us_counties.countycd
         LEFT JOIN ecological_groups
-        ON ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name
-        GROUP BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
-        ORDER BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
+        ON 
+            (ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+            OR (ecological_groups.species = chang_fips.species)
+            OR (LOWER(REGEXP_REPLACE(phillips_locations.speciesbinomial, '.*\s', '')) = LOWER(ecological_groups.species))
+        GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+        ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
     )
     SELECT statecd, countycd, BOOL_OR(is_epigeic_row) AS has_epigeic
     FROM combos
@@ -65,19 +108,34 @@ output_2 = pd.merge(output_1, epigeics_df, on=['statecd', 'countycd'], how="left
 cursor.execute(f"""
     WITH 
     combos AS (
+        WITH 
+        phillips_locations AS (
+            SELECT phillips_sites_fips.site_name, originalspeciesbinomial, speciesbinomial, phillips_sites_fips.statecd, phillips_sites_fips.countycd
+            FROM phillips_sites_fips
+            LEFT JOIN worm_sp_occur_phillips 
+            ON phillips_sites_fips.site_name = worm_sp_occur_phillips.site_name
+            WHERE worm_sp_occur_phillips.native = 'Non-native'
+        )
         -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they equal anecic)
         SELECT 
-            east_us_plot.statecd, 
-            east_us_plot.countycd, 
+            east_us_counties.statecd, 
+            east_us_counties.countycd, 
             ecological_groups.ecological_group,
             COALESCE(ecological_groups.ecological_group = 'Anecic', false) AS is_anecic_row
-        FROM east_us_plot
+        FROM east_us_counties
         LEFT JOIN worms_drake 
-        ON east_us_plot.statecd = worms_drake.statecd AND east_us_plot.countycd = worms_drake.countycd
+        ON worms_drake.origin != 'Nearctic' AND east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+        LEFT JOIN chang_fips
+        ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+        LEFT JOIN phillips_locations
+        ON phillips_locations.statecd = east_us_counties.statecd AND phillips_locations.countycd = east_us_counties.countycd
         LEFT JOIN ecological_groups
-        ON ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name
-        GROUP BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
-        ORDER BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
+        ON 
+            (ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+            OR (ecological_groups.species = chang_fips.species)
+            OR (LOWER(REGEXP_REPLACE(phillips_locations.speciesbinomial, '.*\s', '')) = LOWER(ecological_groups.species))
+        GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+        ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
     )
     SELECT statecd, countycd, BOOL_OR(is_anecic_row) AS has_anecic
     FROM combos
@@ -95,19 +153,34 @@ output_3 = pd.merge(output_2, anecic_df, on=["statecd", "countycd"], how="left")
 cursor.execute(f"""
     WITH 
     combos AS (
+        WITH 
+        phillips_locations AS (
+            SELECT phillips_sites_fips.site_name, originalspeciesbinomial, speciesbinomial, phillips_sites_fips.statecd, phillips_sites_fips.countycd
+            FROM phillips_sites_fips
+            LEFT JOIN worm_sp_occur_phillips 
+            ON phillips_sites_fips.site_name = worm_sp_occur_phillips.site_name
+            WHERE worm_sp_occur_phillips.native = 'Non-native'
+        )
         -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they equal endogeic)
         SELECT 
-            east_us_plot.statecd, 
-            east_us_plot.countycd, 
+            east_us_counties.statecd, 
+            east_us_counties.countycd, 
             ecological_groups.ecological_group,
             COALESCE(ecological_groups.ecological_group = 'Endogeic', false) AS is_endogeic_row
-        FROM east_us_plot
+        FROM east_us_counties
         LEFT JOIN worms_drake 
-        ON east_us_plot.statecd = worms_drake.statecd AND east_us_plot.countycd = worms_drake.countycd
+        ON worms_drake.origin != 'Nearctic' AND east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+        LEFT JOIN chang_fips
+        ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+        LEFT JOIN phillips_locations
+        ON phillips_locations.statecd = east_us_counties.statecd AND phillips_locations.countycd = east_us_counties.countycd
         LEFT JOIN ecological_groups
-        ON ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name
-        GROUP BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
-        ORDER BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
+        ON 
+            (ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+            OR (ecological_groups.species = chang_fips.species)
+            OR (LOWER(REGEXP_REPLACE(phillips_locations.speciesbinomial, '.*\s', '')) = LOWER(ecological_groups.species))
+        GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+        ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
     )
     SELECT statecd, countycd, BOOL_OR(is_endogeic_row) AS has_endogeic
     FROM combos
@@ -125,19 +198,34 @@ output_4 = pd.merge(output_3, endogeic_df, on=["statecd", "countycd"], how="left
 cursor.execute(f"""
     WITH 
     combos AS (
+        WITH 
+        phillips_locations AS (
+            SELECT phillips_sites_fips.site_name, originalspeciesbinomial, speciesbinomial, phillips_sites_fips.statecd, phillips_sites_fips.countycd
+            FROM phillips_sites_fips
+            LEFT JOIN worm_sp_occur_phillips 
+            ON phillips_sites_fips.site_name = worm_sp_occur_phillips.site_name
+            WHERE worm_sp_occur_phillips.native = 'Non-native'
+        )
         -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they equal epi-endogeic)
         SELECT 
-            east_us_plot.statecd, 
-            east_us_plot.countycd, 
+            east_us_counties.statecd, 
+            east_us_counties.countycd, 
             ecological_groups.ecological_group,
             COALESCE(ecological_groups.ecological_group = 'Epi-Endogeic', false) AS is_epiendogeic_row
-        FROM east_us_plot
+        FROM east_us_counties
         LEFT JOIN worms_drake 
-        ON east_us_plot.statecd = worms_drake.statecd AND east_us_plot.countycd = worms_drake.countycd
+        ON worms_drake.origin != 'Nearctic' AND east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+        LEFT JOIN chang_fips
+        ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+        LEFT JOIN phillips_locations
+        ON phillips_locations.statecd = east_us_counties.statecd AND phillips_locations.countycd = east_us_counties.countycd
         LEFT JOIN ecological_groups
-        ON ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name
-        GROUP BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
-        ORDER BY east_us_plot.statecd, east_us_plot.countycd, ecological_groups.ecological_group
+        ON 
+            (ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+            OR (ecological_groups.species = chang_fips.species)
+            OR (LOWER(REGEXP_REPLACE(phillips_locations.speciesbinomial, '.*\s', '')) = LOWER(ecological_groups.species))
+        GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+        ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
     )
     SELECT statecd, countycd, BOOL_OR(is_epiendogeic_row) AS has_epiendogeic
     FROM combos
