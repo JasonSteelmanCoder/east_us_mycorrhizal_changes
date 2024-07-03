2cb59e663b26362ec42369fc0ca27174dd21c256
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-07-03 09:17:17 -0400

    rewrite worm queries to include chang and phillips data

--- SQL_snippets/make_east_us_counties_table_from_county_fips_table.sql
+++ SQL_snippets/make_east_us_counties_table_from_county_fips_table.sql
@@ -4,7 +4,7 @@
 -- For this program to run properly, download county_fips_master.csv from https://github.com/kjhealy/fips-codes/blob/master/county_fips_master.csv, 
 -- then, copy the csv into your database as county_fips.
 
-CREATE TABLE east_us_fips
+CREATE TABLE east_us_counties
 AS (
   SELECT fips, county_name, state_abbr, state_name, state AS statecd, county AS countycd, region_name, division_name
   FROM county_fips
@@ -37,6 +37,20 @@ AS (
 		'Wisconsin'
   )
 );
+
+-- Delete a city that merged into another county and therefore has 'NA' for statecd and countycd
+DELETE FROM east_us_counties WHERE county_name = 'Bedford city' AND state_abbr = 'VA'
+
+-- cast statecd and countycd as smallint for consistency with other tables
+ALTER TABLE east_us_counties
+ALTER COLUMN statecd
+TYPE smallint
+USING statecd::smallint;
+
+ALTER TABLE east_us_counties
+ALTER COLUMN countycd
+TYPE smallint
+USING countycd::smallint;
 
 -- Clean up by deleting the original table from the database:
 
--- SQL_snippets/scratch.sql
+++ SQL_snippets/scratch.sql
@@ -1,44 +1,26 @@
-ROUND(
-    (
-        (
-            SUM(CASE WHEN association = 'EM' AND fire_classification IS NOT NULL THEN basal_area END) 
-            
-            + 
-            
-            (
-                0.5 
-                
-                * 
-                
-                COALESCE(
-                    
-                    SUM(CASE WHEN association = 'AM-EM' AND fire_classification IS NOT NULL THEN basal_area END)
-                    
-                    , 
-                    
-                    0)
-                
-            ) 
-        )
-
-        / 
-        
-        (
-            SUM(CASE WHEN association = 'EM' AND fire_classification IS NOT NULL THEN basal_area END) 
-            
-            + 
-            
-            SUM(CASE WHEN association = 'AM' AND fire_classification IS NOT NULL THEN basal_area END) 
-            
-            + 
-            
-            SUM(CASE WHEN association = 'AM-EM' AND fire_classification IS NOT NULL THEN basal_area END)
-            
-        ) 
+    WITH 
+    combos AS (
+          -- find all combinations of counties and ecoregions (ecoregions are displayed as booleans for whether they equal epi-endogeic)
+          SELECT 
+              east_us_counties.statecd, 
+              east_us_counties.countycd, 
+              ecological_groups.ecological_group,
+              COALESCE(ecological_groups.ecological_group = 'Epi-Endogeic', false) AS is_epiendogeic_row
+          FROM east_us_counties
+          LEFT JOIN worms_drake 
+          ON east_us_counties.statecd = worms_drake.statecd AND east_us_counties.countycd = worms_drake.countycd
+      		LEFT JOIN chang_fips
+      		ON chang_fips.statecd = east_us_counties.statecd AND chang_fips.countycd = east_us_counties.countycd
+          LEFT JOIN ecological_groups
+          ON 
+      			(ecological_groups.family = worms_drake.family AND ecological_groups.name = worms_drake.name) 
+      			OR (ecological_groups.species = chang_fips.species)
+          GROUP BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
+          ORDER BY east_us_counties.statecd, east_us_counties.countycd, ecological_groups.ecological_group
     )
-
-    * 
+    SELECT statecd, countycd, BOOL_OR(is_epiendogeic_row) AS has_epiendogeic
+    FROM combos
+    GROUP BY statecd, countycd
+    ORDER BY statecd, countycd;
     
-    100
     
-, 1) AS pct_em,
