738305675392db57b2cced0e51a8ecdd913cca08
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-07-25 09:17:07 -0400

    turn query result column into a list that can be used in an IN statement

---
+++ python_programs/listifier.py
@@ -0,0 +1,30 @@
+copied_list = '''PA
+NH
+MD
+IL
+TN
+OH
+KY
+NY
+NJ
+MI
+MA
+AL
+MS
+IN
+WV
+VT
+FL
+ME
+SC
+WI
+VA
+NC
+CT
+DE
+RI
+GA'''
+
+new_list = tuple(copied_list.split('\n'))
+
+print(new_list)
