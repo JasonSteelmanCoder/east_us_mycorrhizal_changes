b5396436fec24ffd03bfa35ea0e7a97be696a7c8
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-07-29 13:12:40 -0400

    style pca for legibility

--- python_programs/pca_of_fire_characteristics.py
+++ python_programs/pca_of_fire_characteristics.py
@@ -7,6 +7,7 @@ from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import os
 from dotenv import load_dotenv
+from adjustText import adjust_text
 
 load_dotenv()
 
@@ -14,8 +15,8 @@ df = pd.read_csv(f'C:/Users/{os.getenv('MS_USER_NAME')}/Desktop/pca_input_7_29_2
 
 X = df.iloc[:, 2:7].values
 colors = df.iloc[:, 0].values
-sizes = df.iloc[:, 7] * 0.5
-# labels = df.iloc[:, 1]
+sizes = df.iloc[:, 8]
+labels = df.iloc[:, 1]
 
 X_standardized = StandardScaler().fit_transform(X)
 
@@ -23,9 +24,11 @@ pca = PCA(n_components=2)
 principal_components = pca.fit_transform(X_standardized)
 
 plt.figure(figsize = (10, 8))
-plt.scatter(principal_components[:, 0], principal_components[:, 1], c = colors, cmap = 'viridis', edgecolor='k', s=sizes)
-# for i, label in enumerate(labels):
-#     plt.annotate(label, (principal_components[i, 0], principal_components[i, 1]), (principal_components[i, 0] + 0.2, principal_components[i, 1]), fontsize = 7)
+plt.scatter(principal_components[:, 0], principal_components[:, 1], c = colors, edgecolor='k', s=sizes * 0.01, linewidths=0.5)
+texts = []
+for i, label in enumerate(labels):
+    texts.append(plt.annotate(label, (principal_components[i, 0], principal_components[i, 1]), fontsize = 7))
+adjust_text(texts, only_move={'texts':'xy'})
 plt.xlabel('PC1')
 plt.ylabel('PC2')
 plt.title('PCA of fire characteristics of various common species')
