f549751da0e96dd9c83363e2d2e16f201cd548d0
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-08-06 13:12:49 -0400

    measure accuracy of model

--- python_programs/decision_tree_for_fire_classification.py
+++ python_programs/decision_tree_for_fire_classification.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
 from sklearn.inspection import permutation_importance
+from sklearn.model_selection import LeaveOneOut, KFold, cross_val_score
 
 load_dotenv()
 
@@ -29,21 +30,31 @@ encoded_target = le.transform(target)
 clf = DecisionTreeClassifier()
 clf.fit(encoded_features, encoded_target)
 
-imp_data = permutation_importance(clf, encoded_features, encoded_target, n_repeats=700, n_jobs=-1)
-importances = imp_data.importances_mean
-print('\n')
-print("IMPORTANCE SCORES")
-print('\n')
-print(f"flame_duration_s: \t\t{importances[0]}")
-print(f"percent_consumed: \t\t{importances[1]}")
-print(f"mean_litter_k: \t\t\t{importances[2]}")
-print(f"mean_litter_cn: \t\t{importances[3]}")
-print(f"bark_diameter_ratio: \t\t{importances[4]}")
-print(f"bark_vol_percent: \t\t{importances[5]}")
-print(f"smoulder_duration: \t\t{importances[6]}")
-print('\n')
-
 # new_instance = [54,88,0.4,None,0.0786,19.12,240]
 # predicted_association = clf.predict([new_instance])
 # decoded_predicted_association = le.inverse_transform(predicted_association)
 # print(decoded_predicted_association)
+
+# imp_data = permutation_importance(clf, encoded_features, encoded_target, n_repeats=100000, n_jobs=-1)       # 100,000 repeats = ~5 minutes runtime
+# importances = imp_data.importances_mean
+# print('\n')
+# print("IMPORTANCE SCORES FOR FIRE ADAPTATION")
+# print('\n')
+# print(f"flame_duration_s: \t\t{importances[0]}")
+# print(f"percent_consumed: \t\t{importances[1]}")
+# print(f"mean_litter_k: \t\t\t{importances[2]}")
+# print(f"mean_litter_cn: \t\t{importances[3]}")
+# print(f"bark_diameter_ratio: \t\t{importances[4]}")
+# print(f"bark_vol_percent: \t\t{importances[5]}")
+# print(f"smoulder_duration: \t\t{importances[6]}")
+# print('\n')
+
+loo_model = DecisionTreeClassifier()
+loo = LeaveOneOut()
+loo_scores = cross_val_score(loo_model, encoded_features, encoded_target, cv=loo)
+print(f"LOO Accuracy for fire adaptation: {loo_scores.mean() * 100}%")
+
+kf_model = DecisionTreeClassifier()
+kf = KFold(n_splits=3, shuffle=True, random_state=1)
+kf_scores = cross_val_score(kf_model, encoded_features, encoded_target, cv=kf)
+print(f"k-Fold Accuracy for fire adaptation: {kf_scores.mean() * 100}%")
