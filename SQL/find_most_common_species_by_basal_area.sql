1e70095a10388cc980b46139c65142655d53045a
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-11-06 15:07:48 -0500

    filter for timberland

--- SQL/find_most_common_species_by_basal_area.sql
+++ SQL/find_most_common_species_by_basal_area.sql
@@ -37,13 +37,13 @@ CREATE TABLE fire_adaptation_2 AS (
     SELECT east_us_tree.spcd, ROUND(SUM(((PI() * (east_us_tree.dia::decimal * 2.54 / 2) ^ 2) / 10000)::decimal), 4) AS basal_area
     FROM east_us_tree 
     LEFT JOIN east_us_cond
-    ON east_us_tree.statecd = east_us_cond.statecd AND east_us_tree.unitcd = east_us_cond.unitcd AND east_us_tree.countycd = east_us_cond.countycd AND CAST(east_us_tree.plot AS text) = east_us_cond.plot AND east_us_tree.invyr = east_us_cond.invyr AND east_us_tree.condid = east_us_cond.condid
+    ON east_us_tree.statecd = east_us_cond.statecd AND east_us_tree.unitcd = east_us_cond.unitcd AND east_us_tree.countycd = east_us_cond.countycd AND east_us_tree.plot = east_us_cond.plot AND east_us_tree.invyr = east_us_cond.invyr AND east_us_tree.condid = east_us_cond.condid
 
     WHERE 
 
     east_us_tree.invyr > 2014 AND east_us_tree.invyr < 2023 AND									-- at T2
 
-    (east_us_cond.industrialcd_fiadb IS NULL OR east_us_cond.industrialcd_fiadb = 0) AND	-- exclude timberland            
+    east_us_cond.stdorgcd = 0 AND	-- exclude timberland            
     east_us_tree.statuscd = 1			-- only count live trees            
 
     GROUP BY east_us_tree.spcd
